4b2d893d918d3e26f269e14cc76f139eb377e244
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-02-15 00:13:47 +0530

    Halfway there

--- JS/app.js
+++ JS/app.js
@@ -29,3 +29,44 @@ const data = [
     }
 ];
 
+// => CV Iterator
+function cvItertorNext(profiles) {
+    let nextIndex=0;
+    return {
+        next: function() {
+            // --> Using ternary operator.
+            return (nextIndex<profiles.length) ? {value: profiles[nextIndex++], done: false} : {done: true};
+        }
+    }
+}
+
+function cvItertorPrevious(profiles, currentIndex) {
+    let nextIndex=currentIndex;
+    return {
+        next: function() {
+            // --> Using ternary operator.
+            return (nextIndex<profiles.length) ? {value: profiles[--nextIndex], done: false} : {done: true};
+        }
+    }
+}
+
+// => Button listner for next button
+let next = document.getElementById('next');
+let previous = document.getElementById('previous');
+
+next.addEventListener('click', nextCV);
+previous.addEventListener('click', previousCV);
+
+function nextCV() {
+    let img = document.getElementById('img');
+    let profile = document.getElementById('profile');
+    img.innerHTML = `<img scr=''></img>`;
+    profile.innerHTML = ``;
+    
+}
+
+function previousCV() {
+    let img = document.getElementById('img');
+    let profile = document.getElementById('profile');
+
+}
