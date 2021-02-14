3ef4980048cae23ccf8d9ea3bf0140a70ad698fc
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-02-14 23:53:23 +0530

    Created sample data which can be fetched from an API

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,31 @@
 console.log("Hello world.");
 
+// Array of objects which contain information about the candidates.
+
+const data = [
+    {
+        name: 'Rohan Das',
+        age: 21,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/75.jpg'
+    },
+    {
+        name: 'Anil Kapoor',
+        age: 23,
+        city: 'Kolkata',
+        language: 'JavaScript',
+        framework: 'Vanilla',
+        image: 'https://randomuser.me/api/portraits/men/72.jpg'
+    },
+    {
+        name: 'Sanjay Kapoor',
+        age: 32,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Flask',
+        image: 'https://randomuser.me/api/portraits/men/63.jpg'
+    }
+];
+
