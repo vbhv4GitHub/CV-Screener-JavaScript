357999a9498de1eca75db200ba034dfe50db96d5
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-02-15 22:46:19 +0530

    Previous Button working smooth. Minimal functionality ready.

--- JS/app.js
+++ JS/app.js
@@ -29,46 +29,53 @@ const data = [
     }
 ];
 
+let currentIndex=-1;
+
 // => CV Iterator
-function cvItertorNext(profiles) {
-    let nextIndex=0;
+function cvIteratorNext(profiles) {
+    let nextIndex = 0;
     return {
-        next: function() {
+        next: function () {
+            currentIndex++;
+            nextIndex = currentIndex;
             // --> Using ternary operator.
-            return nextIndex<profiles.length ? {value: profiles[nextIndex++], done: false} : {done: true};
+            return nextIndex < profiles.length ? { value: profiles[nextIndex], done: false } : { done: true };
         }
     }
 }
 
-function cvItertorPrevious(profiles, currentIndex) {
-    let nextIndex=currentIndex;
+function cvIteratorPrevious(profiles) {
+    let nextIndex2 = currentIndex;
+    console.log("Defining cvInteratorPrevious Here.");
     return {
-        next: function() {
-            // --> Using ternary operator.
-            return (nextIndex<profiles.length) ? {value: profiles[--nextIndex], done: false} : {done: true};
+        previous: function () {
+            currentIndex--;
+            nextIndex2 = currentIndex;
+            return (nextIndex2 <= profiles.length) ? { value: profiles[nextIndex2], done: false } : { done: true };
         }
     }
 }
 
 // => Button listner for next and previous buttons
 let next = document.getElementById('next');
-let previous = document.getElementById('previous');
 next.addEventListener('click', nextCV);
+let previous = document.getElementById('previous');
 previous.addEventListener('click', previousCV);
 
 
 // Variable to access the profile data of the candidates.
-const candidates = cvItertorNext(data);
+const candidates = cvIteratorNext(data);
+const candidates2 = cvIteratorPrevious(data);
 
 nextCV();
 
 function nextCV() {
     const currentCandidate = candidates.next().value;
-    if(currentCandidate != undefined) {
-    let img = document.getElementById('img');
-    let profile = document.getElementById('profile');
-    img.innerHTML = `<img src='${currentCandidate.image}'>`;
-    profile.innerHTML = `<ul class="list-group list-group-flush">
+    if (currentCandidate != undefined) {
+        let img = document.getElementById('img');
+        let profile = document.getElementById('profile');
+        img.innerHTML = `<img src='${currentCandidate.image}'>`;
+        profile.innerHTML = `<ul class="list-group list-group-flush">
                             <li class="list-group-item">Name: ${currentCandidate.name}</li>
                             <li class="list-group-item">Age: ${currentCandidate.age}</li>
                             <li class="list-group-item">Location: ${currentCandidate.city}</li>
@@ -77,26 +84,29 @@ function nextCV() {
                         </ul>`;
     }
     else {
-        alert("The end of applications.");
+        alert("You've reached the last application.");
         location.reload();
     }
-    
+
 }
 
 function previousCV() {
-    // Variable to access the profile data of the candidates.
-    const candidates = cvItertorNext(data);
-    const currentCandidate = candidates.next.value();
-
-    let img = document.getElementById('img');
-    let profile = document.getElementById('profile');
-    img.innerHTML = `<img src='${currentCandidate.image}'></img>`;
-    profile.innerHTML = `<ul class="list-group list-group-flush">
+    const currentCandidate = candidates2.previous().value;
+    if (currentCandidate != undefined) {
+        let img = document.getElementById('img');
+        let profile = document.getElementById('profile');
+        img.innerHTML = `<img src='${currentCandidate.image}'>`;
+        profile.innerHTML = `<ul class="list-group list-group-flush">
                             <li class="list-group-item">Name: ${currentCandidate.name}</li>
                             <li class="list-group-item">Age: ${currentCandidate.age}</li>
                             <li class="list-group-item">Location: ${currentCandidate.city}</li>
                             <li class="list-group-item">Language: ${currentCandidate.language}</li>
                             <li class="list-group-item">Framework: ${currentCandidate.framework}</li>
                         </ul>`;
-    
+    }
+    else {
+        alert("You've reached first application.");
+        location.reload();
+    }
+
 }
